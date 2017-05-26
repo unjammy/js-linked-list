f14a542fe01f0570bfd996b17481354e5ecad375
clarkdenunzio <clark@clarkdenunzio.com>
2017-05-25 20:35:46 -1000

    done and ready for testing

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -86,30 +86,62 @@ function doublyLinkedListGenerator(){
   };
 
   var _remove = function( index ) {
+
     var toRemove = head;
+
     if( index >= size ){
+
       return false;
+
     } else {
+
       for(var i = 0; i < index; i++){
         toRemove = toRemove.next;
       }
+
       toRemove.prev.next = toRemove.next;
       toRemove.next.prev = toRemove.prev;
       size--;
+
     }
 
   };
 
   var _insert = function( index, value ){
+
     var toInsert = {};
+
     if( index >= size || index < 0 ){
+
       return false;
+
     } else if( index === 0 ) {
+
       toInsert = _node( value );
-      head.prev
+      head.prev = toInsert;
+      toInsert.next = head;
+      head = toInsert;
+
     } else if( index === size ) {
-      tail.next =
+
+      toInsert = _node( value );
+      tail.next = toInsert;
+      toInsert.prev = tail;
+      tail = toInsert;
+
     } else {
+
+      var cursor = head;
+
+      for(var i = 0; i < index; i++){
+        cursor = cursor.next;
+      }
+
+      toInsert = _node( value );
+      toInsert.prev = cursor;
+      toInsert.next = cursor.next;
+      cursor.next.prev = toInsert;
+      cursor.next = toInsert;
 
     }
   };
